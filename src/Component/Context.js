567a8ba5e8f04460da9b9582f0194bb2bb91882b
gaubacuc56 <76199289+gaubacuc56@users.noreply.github.com>
2022-01-03 13:22:41 +0700

    commit

--- src/Component/Context.js
+++ src/Component/Context.js
@@ -6,21 +6,21 @@ export const AccountContext = React.createContext("2");
 
 
 export class DataProvider extends Component {
-    
+
     state = {
         products: [],
         clients: [],
-        cart : [],
-        account:[],
+        cart: [],
+        account: [],
         order_history: [],
         total: 0
     }
-    componentDidMount(){
+    componentDidMount() {
         axios({
             method: 'GET',
-            url: 'https://localhost:44328/api/Products/GetAll',
+            url: 'http://192.168.1.54:8082/api/Products/GetAll',
             data: null
-        }).then(res =>{
+        }).then(res => {
             this.setState({
                 products: res.data
             })
@@ -30,58 +30,58 @@ export class DataProvider extends Component {
             method: 'GET',
             url: 'https://localhost:44328/api/Customers/GetAll',
             data: null
-        }).then(res =>{
+        }).then(res => {
             this.setState({
                 clients: res.data
             })
         })
     }
-    
-   /*  componentWillUpdate() { // immmediately update employee after signup without refreshing
-        callApi('employee', "GET", null).then(res=>{
-            this.setState({
-                employee: res.data
-            })
-        })
 
-    } */
-    
-    addtoCart = (id, color, size) =>{
-        
-        const {products, cart} = this.state;
-        if (color.length === 0 && size.length === 0 )
+    /*  componentWillUpdate() { // immmediately update employee after signup without refreshing
+         callApi('employee', "GET", null).then(res=>{
+             this.setState({
+                 employee: res.data
+             })
+         })
+ 
+     } */
+
+    addtoCart = (id, color, size) => {
+
+        const { products, cart } = this.state;
+        if (color.length === 0 && size.length === 0)
             alert("Vui lòng chọn màu và size")
         else if (color.length === 0)
             alert("Vui lòng chọn màu")
         else if (size.length === 0)
             alert("Vui lòng chọn size")
         else if (color.length !== 0 && size.length !== 0) {
-            const check = cart.every(item=>{
+            const check = cart.every(item => {
                 if (id === item.id && color === item.product_detail_color && item.product_detail_size === size)
                     return false;
-                else return true                   
+                else return true
             })
-                 
+
             if (check) {
-                const data = products.filter(item=>{
+                const data = products.filter(item => {
                     return item.id === id
                 })
                 const array = data
                 const index = cart.length;
-                const quantity = 1; 
+                const quantity = 1;
                 const product_detail_color = color
                 const product_detail_size = size
                 this.setState({
-                    cart: [...cart, {index, array, id, product_detail_color, product_detail_size, quantity}]
+                    cart: [...cart, { index, array, id, product_detail_color, product_detail_size, quantity }]
                 })
-                         
+
                 alert("Thêm sản phẩm thành công")
-    
+
                 console.log(cart);
             }
             else alert("Sản phẩm đã tồn tại trong giỏ hàng")
         }
-        
+
     }
 
     getPrice = (id) => {
@@ -96,114 +96,114 @@ export class DataProvider extends Component {
 
         return str_price;
     }
-    increase = (index) =>{
-        const {cart} = this.state;
-        cart.forEach(item=>{
+    increase = (index) => {
+        const { cart } = this.state;
+        cart.forEach(item => {
             if (item.index === index)
                 item.quantity += 1;
         })
         this.getTotal();
-        this.setState({cart: cart})
+        this.setState({ cart: cart })
     }
 
-    decrease = (index) =>{
-        const {cart} = this.state;
-        cart.forEach(item=>{
+    decrease = (index) => {
+        const { cart } = this.state;
+        cart.forEach(item => {
             if (item.index === index)
-                item.quantity === 1 ? item.quantity = 1 : item.quantity -=1;
+                item.quantity === 1 ? item.quantity = 1 : item.quantity -= 1;
         })
         this.getTotal();
-        this.setState({cart: cart})
+        this.setState({ cart: cart })
     }
 
-    remove = (indexOfitem) =>{
-        const {cart} = this.state;
-        cart.forEach((item, index)=>{
+    remove = (indexOfitem) => {
+        const { cart } = this.state;
+        cart.forEach((item, index) => {
             if (item.index === indexOfitem)
                 cart.splice(index, 1)
         })
         this.getTotal();
-        this.setState({cart: cart})
+        this.setState({ cart: cart })
     }
 
-    getMultiplePrice = (id, quantity) =>{
+    getMultiplePrice = (id, quantity) => {
         let str_price = "";
         this.state.products.forEach((item) => {
             if (item.id === id)
-                str_price = (item.product_price*quantity).toString()
+                str_price = (item.product_price * quantity).toString()
         })
         if (str_price.length === 7) {
             //Chuõi giá tiền = 7 thì chèn , vô chỗ số 1 và 5
             str_price = str_price.slice(0, 1) + "," + str_price.slice(1)
             str_price = str_price.slice(0, 5) + "," + str_price.slice(5)
         }
-        else  if (str_price.length > 7){
+        else if (str_price.length > 7) {
             //Chuỗi giá tiền > 7 thì chèn , vô chỗ số 1 + i và 5 + i
             let i = str_price.length - 7
-            str_price = str_price.slice(0, 1+i) + "," + str_price.slice(1+i)
-            str_price = str_price.slice(0, 5+i) + "," + str_price.slice(5+i)
-        } 
+            str_price = str_price.slice(0, 1 + i) + "," + str_price.slice(1 + i)
+            str_price = str_price.slice(0, 5 + i) + "," + str_price.slice(5 + i)
+        }
 
         return str_price;
     }
 
-    getTotal = () =>{
-        const {cart} = this.state;
+    getTotal = () => {
+        const { cart } = this.state;
         if (cart.length == 0) {
             this.setState({
                 total: 0
-            }) 
+            })
         }
         else {
             let res = 0;
             cart.forEach((cartitem) => {
-                cartitem.array.forEach(item =>{
+                cartitem.array.forEach(item => {
                     res = res + (item.product_price * cartitem.quantity)
-                })             
-            })    
-            let str_price = res.toString(); 
-            
+                })
+            })
+            let str_price = res.toString();
+
             if (str_price.length === 7) {
                 //Chuõi giá tiền = 7 thì chèn , vô chỗ số 1 và 5
                 str_price = str_price.slice(0, 1) + "," + str_price.slice(1)
                 str_price = str_price.slice(0, 5) + "," + str_price.slice(5)
             }
-            else  if (str_price.length > 7){
+            else if (str_price.length > 7) {
                 //Chuỗi giá tiền > 7 thì chèn , vô chỗ số 1 + i và 5 + i
                 let i = str_price.length - 7
-                str_price = str_price.slice(0, 1+i) + "," + str_price.slice(1+i)
-                str_price = str_price.slice(0, 5+i) + "," + str_price.slice(5+i)
-            } 
-            
-            
+                str_price = str_price.slice(0, 1 + i) + "," + str_price.slice(1 + i)
+                str_price = str_price.slice(0, 5 + i) + "," + str_price.slice(5 + i)
+            }
+
+
             this.setState({
                 total: str_price
-            }) 
+            })
         }
-        
+
     }
     Login = (username, password) => {
-        const { clients} = this.state
+        const { clients } = this.state
         const data = clients.filter(item => {
-            if  ( username === item.customer_phoneNumber || username === item.customer_email  && password===item.customer_password )
+            if (username === item.customer_phoneNumber || username === item.customer_email && password === item.customer_password)
                 return item
         })
         this.setState({
-            account: {data}
+            account: { data }
         })
-    
+
     }
-    Logout = () =>{
-        const { account} = this.state
+    Logout = () => {
+        const { account } = this.state
         this.setState({
-            account : {}
+            account: {}
         })
-        
+
     }
-    ClearCart = () =>{
-        const { cart} = this.state
+    ClearCart = () => {
+        const { cart } = this.state
         this.setState({
-            cart : []
+            cart: []
         })
     }
 
@@ -224,12 +224,12 @@ export class DataProvider extends Component {
         })
         return url
     }
- 
+
     render() {
-        const {products, cart, account, clients, total, order_history} = this.state;
-        const {addtoCart, getPrice, increase, decrease, remove, getMultiplePrice, getTotal ,Login, Logout, ClearCart, getNamebyID, getImgbyID} = this;
+        const { products, cart, account, clients, total, order_history } = this.state;
+        const { addtoCart, getPrice, increase, decrease, remove, getMultiplePrice, getTotal, Login, Logout, ClearCart, getNamebyID, getImgbyID } = this;
         return (
-            <DataContext.Provider value={{products,cart,account, clients, total, order_history, addtoCart, getPrice,increase, decrease, remove, getMultiplePrice, getTotal,Login, Logout, ClearCart, getNamebyID, getImgbyID} }>
+            <DataContext.Provider value={{ products, cart, account, clients, total, order_history, addtoCart, getPrice, increase, decrease, remove, getMultiplePrice, getTotal, Login, Logout, ClearCart, getNamebyID, getImgbyID }}>
                 {this.props.children}
             </DataContext.Provider>
         );
